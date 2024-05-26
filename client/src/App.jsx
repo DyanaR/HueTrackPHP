afc92cb90a15be4119100b174c995f1ca2aa1823
DyanaR <dyanarahhal98@gmail.com>
2024-05-26 05:08:06 -0500

    Add assets and folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,44 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import HabitTracker from "./pages/HabitTracker";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import styled from "styled-components";
+import { AuthContextProvider } from "./context/AuthContext";
+import ProtectedRoutes from "./pages/ProtectedRoutes";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Container>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/HueTrack"
+            element={
+              <ProtectedRoutes>
+                <HabitTracker />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/Account"
+            element={
+              <ProtectedRoutes>
+                <Account />
+              </ProtectedRoutes>
+            }
+          />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+        </Routes>
+      </AuthContextProvider>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div``;
